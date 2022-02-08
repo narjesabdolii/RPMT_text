e21f4cd49568fa8a644c4f9b903c8a7aae164e0e
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-08 18:30:21 +0330

    update homepage by backgound and text

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:persian_fonts/persian_fonts.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,6 +12,123 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RPMT',
+          style: TextStyle(
+            fontFamily: 'IndieFlower-Regular',
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.teal[400],
+        centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (context){
+              return [
+                const PopupMenuItem<int>(
+                  value: 0,
+                  child: Text('settings'),
+                )
+              ];
+            },
+            onSelected: (value){
+              if(value == 0) {
+                print('settings ');
+              }
+            },
+          )
+        ],
+
+
+      ),
+      body: Container(
+        alignment: const Alignment(-2, 0),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/screen.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.fromLTRB(130.0, 200.0, 20.0, 10.0),
+              child: Text(
+                'در این تست مجموعه ای ',
+                style: PersianFonts.Yekan.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
+              child:  Text(
+                'از تصاویر به فرد ارائه ',
+                style: PersianFonts.Yekan.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(95.0, 0.0, 20.0, 10.0),
+              child:  Text(
+                'می شود که باید جای خالی ',
+                style: PersianFonts.Yekan.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
+              child:  Text('را با تصاویر پیشنهادی ',
+                style: PersianFonts.Yekan.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(180.0, 0.0, 20.0, 10.0),
+              child:  Text(' درست پر کند ',
+                style: PersianFonts.Yekan.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(70.0, 10.0, 20.0, 10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    Navigator.pushNamed(context, '/Question_one');
+                  });
+                },
+                child:  Text(
+                  'شروع آزمون',
+                  style: PersianFonts.Yekan.copyWith(
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal[400],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
