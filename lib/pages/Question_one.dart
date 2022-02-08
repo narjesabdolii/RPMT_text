027cbf56a7c9fe949d6876747f5a663097ab94e2
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-08 21:53:17 +0330

    question one page with button and image

--- lib/pages/Question_one.dart
+++ lib/pages/Question_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class QuestionOne extends StatefulWidget {
   const QuestionOne({Key? key}) : super(key: key);
@@ -10,6 +11,113 @@ class QuestionOne extends StatefulWidget {
 class _QuestionOneState extends State<QuestionOne> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text( 'RPMT',
+          style: TextStyle(
+            fontFamily: 'IndieFlower-Regular',
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.teal[700],
+        centerTitle: true,
+      ),
+      body: Container(
+        alignment: const Alignment(-2, 0),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/sc.jpg'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+           Container(
+             padding: const EdgeInsets.all(45.0),
+             child: const Image(
+               image: AssetImage('assets/1.png'),
+             ),
+           ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                    child: ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                      ),
+                      onPressed: () {  },
+                      child: const Text('1'),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                    child: ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                      ),
+                      onPressed: () {  },
+                      child: const Text('2'),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                    child: ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                      ),
+                      onPressed: () {  },
+                      child: const Text('3'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                    child: ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                      ),
+                      onPressed: () {  },
+                      child: const Text('4'),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                    child: ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                      ),
+                      onPressed: () {  },
+                      child: const Text('5'),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                    child: ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                        primary: Colors.teal,
+                      ),
+                      onPressed: () {  },
+                      child: const Text('6'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+
+
+      ),
+    );
   }
 }
