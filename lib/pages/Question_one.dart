733e05d7f53c3c4103efc11243b3449b939750c6
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-08 18:31:47 +0330

    create Question_one.dart

---
+++ lib/pages/Question_one.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionOne extends StatefulWidget {
+  const QuestionOne({Key? key}) : super(key: key);
+
+  @override
+  _QuestionOneState createState() => _QuestionOneState();
+}
+
+class _QuestionOneState extends State<QuestionOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
