091858b225fee84f291c46cd9c6a0925dc08592f
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-08 22:14:37 +0330

    question two page created

---
+++ lib/pages/Question_two.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionTwo extends StatefulWidget {
+  const QuestionTwo({Key? key}) : super(key: key);
+
+  @override
+  _QuestionTwoState createState() => _QuestionTwoState();
+}
+
+class _QuestionTwoState extends State<QuestionTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
