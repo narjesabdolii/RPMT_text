8a1264aca839c76ca344d2c4dbbca189a13fb874
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-09 09:06:37 +0330

    create Question_three.dart file

---
+++ lib/pages/Question_three.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionThree extends StatefulWidget {
+  const QuestionThree({Key? key}) : super(key: key);
+
+  @override
+  _QuestionThreeState createState() => _QuestionThreeState();
+}
+
+class _QuestionThreeState extends State<QuestionThree> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
