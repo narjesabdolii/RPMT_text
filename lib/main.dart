6ffb2ecc240b4c18c966f95bc3745482eb503271
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-09 09:08:28 +0330

    updating routes for Question 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intellejencetest/pages/Home.dart';
 import 'package:intellejencetest/pages/Question_one.dart';
+import 'package:intellejencetest/pages/Question_three.dart';
 import 'package:intellejencetest/pages/Question_two.dart';
 import 'package:intellejencetest/pages/splash.dart';
 
@@ -12,6 +13,7 @@ void main() {
       '/home' : (context) => const Home(),
       '/Question_one' : (context) => const QuestionOne(),
       '/Question_two' : (context)=> const QuestionTwo(),
+      '/Question_three' : (context) => const QuestionThree(),
     },
   ));
 }
