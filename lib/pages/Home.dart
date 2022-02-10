9121f9915f7f3aefbfb97c1b2c83ee7aa06f0699
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-10 16:48:13 +0330

    adding boxdecoration for home

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -54,90 +54,29 @@ class _HomeState extends State<Home> {
                  ],
                 ),
           ),
-          Container(
-            alignment: const Alignment(-2, 0),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/screen.jpg'),
-                fit: BoxFit.fill,
+          const SizedBox(height: 20,),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 40,
+            child:  Container(
+              margin: const EdgeInsets.symmetric(horizontal: 100.0),
+              height: 250,
+              decoration:  BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(35),
+                  boxShadow: const [
+                    BoxShadow(
+                      offset: Offset(0,10),
+                      blurRadius: 50,
+                      color: Colors.amber,
+                    )
+                  ],
+                  image: const DecorationImage(
+                    image: AssetImage('assets/test.png'),
+                  )
               ),
-            ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.fromLTRB(130.0, 200.0, 20.0, 10.0),
-                  child: Text(
-                    'در این تست مجموعه ای ',
-                    style: PersianFonts.Yekan.copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                    textAlign: TextAlign.end,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
-                  child: Text(
-                    'از تصاویر به فرد ارائه ',
-                    style: PersianFonts.Yekan.copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                    textAlign: TextAlign.end,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(95.0, 0.0, 20.0, 10.0),
-                  child: Text(
-                    'می شود که باید جای خالی ',
-                    style: PersianFonts.Yekan.copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                    textAlign: TextAlign.end,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
-                  child: Text('را با تصاویر پیشنهادی ',
-                    style: PersianFonts.Yekan.copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                    textAlign: TextAlign.end,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(180.0, 0.0, 20.0, 10.0),
-                  child: Text(' درست پر کند ',
-                    style: PersianFonts.Yekan.copyWith(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                    textAlign: TextAlign.end,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(70.0, 10.0, 20.0, 10.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        Navigator.pushNamed(context, '/Question_one');
-                      });
-                    },
-                    child: Text(
-                      'شروع آزمون',
-                      style: PersianFonts.Yekan.copyWith(
-                        fontSize: 17.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.teal[400],
-                    ),
-                  ),
-                ),
-              ],
+
             ),
           ),
         ],
