1161d246623f72018bcbfa2f06d7694899e17d13
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-10 15:07:53 +0330

    expand appbar

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:persian_fonts/persian_fonts.dart';
 
@@ -14,91 +15,122 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Container(
-        alignment: const Alignment(-2, 0),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/screen.jpg'),
-            fit: BoxFit.fill,
-          ),
-        ),
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.fromLTRB(130.0, 200.0, 20.0, 10.0),
-              child: Text(
-                'در این تست مجموعه ای ',
-                style: PersianFonts.Yekan.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0,
-                ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
-              child: Text(
-                'از تصاویر به فرد ارائه ',
-                style: PersianFonts.Yekan.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0,
-                ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(95.0, 0.0, 20.0, 10.0),
-              child: Text(
-                'می شود که باید جای خالی ',
-                style: PersianFonts.Yekan.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0,
-                ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
-              child: Text('را با تصاویر پیشنهادی ',
-                style: PersianFonts.Yekan.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0,
-                ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(180.0, 0.0, 20.0, 10.0),
-              child: Text(' درست پر کند ',
-                style: PersianFonts.Yekan.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0,
-                ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(70.0, 10.0, 20.0, 10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    Navigator.pushNamed(context, '/Question_one');
-                  });
-                },
-                child: Text(
-                  'شروع آزمون',
-                  style: PersianFonts.Yekan.copyWith(
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.bold,
+      body: Column(
+        children : <Widget>[
+          Container(
+            height: 130.0,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: 130.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(35),
+                      bottomRight: Radius.circular(35),
+                    ),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Text('RPMT',
+                      style: Theme.of(context).textTheme.headline5?.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                    ],
                   ),
                 ),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.teal[400],
-                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: const Alignment(-2, 0),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/screen.jpg'),
+                fit: BoxFit.fill,
               ),
             ),
-          ],
-        ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.fromLTRB(130.0, 200.0, 20.0, 10.0),
+                  child: Text(
+                    'در این تست مجموعه ای ',
+                    style: PersianFonts.Yekan.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
+                  child: Text(
+                    'از تصاویر به فرد ارائه ',
+                    style: PersianFonts.Yekan.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(95.0, 0.0, 20.0, 10.0),
+                  child: Text(
+                    'می شود که باید جای خالی ',
+                    style: PersianFonts.Yekan.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(90.0, 0.0, 20.0, 10.0),
+                  child: Text('را با تصاویر پیشنهادی ',
+                    style: PersianFonts.Yekan.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(180.0, 0.0, 20.0, 10.0),
+                  child: Text(' درست پر کند ',
+                    style: PersianFonts.Yekan.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(70.0, 10.0, 20.0, 10.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        Navigator.pushNamed(context, '/Question_one');
+                      });
+                    },
+                    child: Text(
+                      'شروع آزمون',
+                      style: PersianFonts.Yekan.copyWith(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.teal[400],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
